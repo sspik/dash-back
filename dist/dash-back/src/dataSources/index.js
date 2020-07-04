b4f0722f6a5b9e20086b90bb5cd9132ffac4c8e5
Oleg <sspik@mail.ru>
2020-07-04 20:24:46 +0300

    remove dist

--- dist/dash-back/src/dataSources/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bitrix24_1 = require("./bitrix24");
-exports.Bitrix = bitrix24_1.Bitrix;
-const yandexMetrika_1 = require("./yandexMetrika");
-exports.YandexMetrika = yandexMetrika_1.YandexMetrika;
-const topvisorApi_1 = require("./topvisorApi");
-exports.Topvisor = topvisorApi_1.Topvisor;
-//# sourceMappingURL=index.js.map
--- dist/dash-back/src/views/getClientCode.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res) => {
-    res.redirect(302, `${process.env.BITRIX24_URL}/oauth/authorize/?client_id=${process.env.CLIENT_ID}`);
-};
-//# sourceMappingURL=getClientCode.js.map
